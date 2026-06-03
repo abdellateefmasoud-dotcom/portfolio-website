b6f2f47ff18ecf3d5d0b0e0e591feb0d52e1900f
Abdellateef <abdellateefmasoud@gmail.com>
2026-06-04 00:34:07 +0400

    updateproject

--- Who-I-Am.js
+++ Who-I-Am.js
@@ -1,16 +1,17 @@
-document.getElementById("btn").onclick = function () {
-  const phone = "0551578833";
-  const email = "25002536@edu.ukcbc.ac.ae";
-
-  document.getElementById("contactInfo").innerHTML =
-    `رقم الهاتف: ${phone} <br> البريد الإلكتروني: ${email}`;
-};
-const skillCards = document.querySelectorAll('#skills .card');
-const certDetails = document.getElementById('certificate-details');
+const skillCards =
+document.querySelectorAll('#skills .card');
 
 skillCards.forEach(card => {
-  card.addEventListener('click', () => {
-    const certInfo = card.getAttribute('data-cert');
-    certDetails.textContent = certInfo;
-  });
+
+card.addEventListener('click', () => {
+
+const cert =
+card.getAttribute('data-cert');
+
+if(cert){
+window.open(cert,'_blank');
+}
+
+});
+
 });
